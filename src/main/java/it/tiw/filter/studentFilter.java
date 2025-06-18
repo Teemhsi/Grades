88f7dc16454dbb8fa28e1cd0ddb9bad1b34fe92f
teemhsi <ishmeets123@gmail.com>
2025-06-18 17:11:01 +0200

    filter fix

---
+++ src/main/java/it/tiw/filter/studentFilter.java
@@ -0,0 +1,38 @@
+package it.tiw.filter;
+
+import it.tiw.beans.Utente;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class studentFilter implements Filter {
+
+    public studentFilter() {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String loginPath = req.getServletContext().getContextPath() + "/login.html";
+        HttpSession session = req.getSession();
+        Utente u =  null;
+        u = (Utente) session.getAttribute("user");
+        if (!u.getRuolo().equalsIgnoreCase("studente")) {
+            res.sendRedirect(loginPath);
+            System.out.print("Login checker FAILED...\n");
+            System.out.print("path: " + loginPath + "\n");
+            return;
+        }
+        System.out.println("logged already");
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
