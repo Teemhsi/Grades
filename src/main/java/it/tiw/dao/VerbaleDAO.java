56dc6a01d58515848e2775f7e3de3f4fe3367f5e
777toma <marco.tomaino@mail.polimi.it>
2025-06-21 19:00:19 +0200

    style fixes main branch

--- src/main/java/it/tiw/dao/VerbaleDAO.java
+++ src/main/java/it/tiw/dao/VerbaleDAO.java
@@ -64,6 +64,7 @@ public class VerbaleDAO {
             }
         }
     }
+
     public String creaVerbaleConIscrizioni(int idAppello) throws SQLException {
         String selectIscrizioniSQL = "SELECT id_studente, voto, stato_valutazione " +
                 "FROM Iscrizioni " +
